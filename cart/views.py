4ec67d978b0b6497815c2eac814e6cfd025c1b79
AlexPullen91 <alex_pullen03@hotmail.com>
2020-12-17 22:50:57 +0000

    Added initial stages of add to cart function in views

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_cart(request):
     """ A view to render shopping cart contents page """
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request):
+    """ Add a quantity of the sweet bags to the shopping cart """
+
+    bag_choice = request.POST.get('bag_choice')
+    redirect_url = request.POST.get('redirect_url')
+
+    print(bag_choice)
+
+    return redirect(redirect_url)
