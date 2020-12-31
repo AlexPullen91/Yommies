fd4232347e41577c37fc0a10585a7fb084c309a5
AlexPullen91 <alex_pullen03@hotmail.com>
2020-12-31 00:42:28 +0000

    Removed old model and replaced lorem conent on index.html

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
-from .models import Card
 
 # Register your models here.
-admin.site.register(Card)
--- home/models.py
+++ home/models.py
@@ -2,10 +2,3 @@ from django.db import models
 
 # Create your models here.
 
-
-class Card(models.Model):
-    name = models.CharField(max_length=254)
-    content = models.TextField()
-
-    def __str__(self):
-        return self.name
