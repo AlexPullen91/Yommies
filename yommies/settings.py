8044d6a99c89375de248b44bacb95181375fd4eb
AlexPullen91 <alex_pullen03@hotmail.com>
2020-12-31 13:40:03 +0000

    secret key removed and debug set to false

--- yommies/settings.py
+++ yommies/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^9@voe&c3n(gibc(6hg*id-i_7ts%j@+www*zg*)++ghit3j6_'
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['yommies.herokuapp.com', 'localhost']
 
