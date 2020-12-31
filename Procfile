da027dcca9c99f44f84e8532fef931238168387b
AlexPullen91 <alex_pullen03@hotmail.com>
2020-12-31 13:27:44 +0000

    added procfile and update requirements.txt ready for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn yommies.wsgi:application
--- yommies/settings.py
+++ yommies/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = '^9@voe&c3n(gibc(6hg*id-i_7ts%j@+www*zg*)++ghit3j6_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yommies.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -123,19 +123,18 @@ WSGI_APPLICATION = 'yommies.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
-POSTGRESDB = os.getenv('POSTGRESDB')
-
-DATABASES = {
-    'default': dj_database_url.parse(POSTGRESDB)
-}
-
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
